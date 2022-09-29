765224c92795f5055fac2371641ecfc03863574a
jeighmz <48871068+jeighmz@users.noreply.github.com>
2022-09-29 18:29:58 -0400

    added optional command line prompt arg for length of lis

--- web2.py
+++ web2.py
@@ -2,46 +2,49 @@ import requests
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import datetime
+import argparse
 
 from time import sleep
 from tqdm import tqdm
 
 now = datetime.datetime.now()
 
+parser = argparse.ArgumentParser()
+
+
 s = HTMLSession()
 content = ''
+
+## optional parameters
+parser.add_argument('--count', type=int, required=False)
+args = parser.parse_args()
+
 
 def extract_news(url):
     token = []
     prices = []
     prices_copy = []
     response = s.get(url)
-    #content = response.content
+    #alternate method: requests
+    ##content = response.content
     soup = BeautifulSoup(response.text, 'html.parser')
-    for i in soup.find_all('td', attrs={'class': "td-price price text-right", 'data-no-decimal': ''}):
+    for i in soup.find_all('td', attrs={'class': "td-price price text-right", 'data-no-decimal': ''}, limit = args.count):
         prices.append(i.text)
-    for j in soup.find_all('span', attrs={'class':"lg:tw-flex font-bold tw-items-center tw-justify-between"}):
+    for j in soup.find_all('span', attrs={'class':"lg:tw-flex font-bold tw-items-center tw-justify-between"}, limit = args.count):
         token.append(j.text)
-    #print(prices)
-    # for elements in prices:
-    #     if '.' not in elements:
-    #         prices.remove(elements)
-    #     else:
-    #         prices2.append(elements)
-    #print(token)
-    #print(len(token), len(prices))
     result = []
+    #cleaning
     for elements in prices:
         e = elements.replace('\n', '')
         prices_copy.append(e)
 
     prices = prices_copy
+    
     if len(prices) == len(token): 
         print("Retrieving Real-time Crypto Data...\n")
     else:
-        #print("Adjusting...")
-        #token = token[6:]
-        print("Retrieving Real-time Crypto Data...")
+        print("Something's Wrong")
+        print("Check lengths: " + len(prices), len(token))
     for i in tqdm(range(len(prices))):
         result.append( token[i][:-1] + ': ' + prices[i] )
     
