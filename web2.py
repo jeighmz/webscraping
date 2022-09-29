dea3a239f27c74cc6803b1368de059b11ed54599
jeighmz <48871068+jeighmz@users.noreply.github.com>
2022-09-29 14:07:47 -0400

    changed to CoinGecko for 100 tokens

--- web2.py
+++ web2.py
@@ -1,4 +1,5 @@
 import requests
+from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import datetime
 
@@ -7,36 +8,48 @@ from tqdm import tqdm
 
 now = datetime.datetime.now()
 
+s = HTMLSession()
 content = ''
 
 def extract_news(url):
     token = []
     prices = []
-    response = requests.get(url)
-    content = response.content
-    soup = BeautifulSoup(content, 'html.parser')
-    for i in soup.find_all('div', attrs={'class': "sc-131di3y-0 cLgOOr", 'style':''}):
+    prices_copy = []
+    response = s.get(url)
+    #content = response.content
+    soup = BeautifulSoup(response.text, 'html.parser')
+    for i in soup.find_all('td', attrs={'class': "td-price price text-right", 'data-no-decimal': ''}):
         prices.append(i.text)
-    for j in soup.find_all('p', attrs={'class' : 'sc-1eb5slv-0 iworPT', 'font-size': '1'}):
+    for j in soup.find_all('span', attrs={'class':"lg:tw-flex font-bold tw-items-center tw-justify-between"}):
         token.append(j.text)
-
+    #print(prices)
+    # for elements in prices:
+    #     if '.' not in elements:
+    #         prices.remove(elements)
+    #     else:
+    #         prices2.append(elements)
+    #print(token)
+    #print(len(token), len(prices))
     result = []
+    for elements in prices:
+        e = elements.replace('\n', '')
+        prices_copy.append(e)
 
+    prices = prices_copy
     if len(prices) == len(token): 
         print("Retrieving Real-time Crypto Data...\n")
     else:
         #print("Adjusting...")
-        token = token[6:]
+        #token = token[6:]
         print("Retrieving Real-time Crypto Data...")
-
     for i in tqdm(range(len(prices))):
-        result.append( token[i] + ': ' + prices[i] )
+        result.append( token[i][:-1] + ': ' + prices[i] )
     
     return result
 
 
-cnt = extract_news('https://coinmarketcap.com')
+cnt = extract_news('https://www.coingecko.com')
 
 for row in cnt:
     print(row)
-    sleep(0.5)
+    sleep(0.35)
--- web4.py
+++ web4.py
@@ -9,7 +9,7 @@ def get_miles(url):
     response = requests.get(url)
     content = response.content
     soup = BeautifulSoup(content, 'html.parser')
-    for i in soup.find_all('span', attrs={"class":"css-901oao css-16my406 r-1vprpcz r-1eo6hi2 r-1b43r93 r-b88u0q r-knv0ih"}):
+    for i in soup.find_all('div', attrs={"class":"css-1dbjc4n r-xoduu5 r-1iusvr4 r-16y2uox r-1wbh5a2"}):
         result.append(i.text)
         #print(i.text)
     return result
